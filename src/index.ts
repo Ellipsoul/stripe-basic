abca127cc55657fa1a3fa8a4573c251f457c5884
aronteh.chess <aronteh.chess@gmail.com>
2024-03-31 15:38:12 +0000

    Add basic Stripe checkout session

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,34 @@ import "dotenv/config";
 
 // Import and instantiate Stripe SDK
 import Stripe from "stripe";
+import { HTTPException } from "hono/http-exception";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 // Start up new Hono backend server
 const app = new Hono();
 
-app.get("/", (c) => c.text("GET it"));
-app.post("/", (c) => c.text("POST it"));
+app.get("/", (c) => c.text("Home"));
+
+// Simple checkout endpoint
+app.post("/checkout", async (c) => {
+  try {
+    // Create a checkout session
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card", "klarna", "ideal"],
+      line_items: [{ price: "price_1P0QCWHWA7Qpg0eCGw6MQOsg", quantity: 1 }],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    });
+
+    return c.json(session);
+  } catch (e: any) {
+    throw new HTTPException(500, { message: e?.message });
+  }
+});
+
+app.get("/success", (c) => c.text("Success!"));
+app.get("/cancel", (c) => c.text("Cancelled!"));
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
