a105f243ce696cd0049e2374be7e1a83894fd776
aronteh.chess <aronteh.chess@gmail.com>
2024-03-31 15:16:24 +0000

    Instantiate Stripe SDK

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import "dotenv/config";
 
+// Import and instantiate Stripe SDK
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+// Start up new Hono backend server
 const app = new Hono();
 
 app.get("/", (c) => c.text("GET it"));
-
 app.post("/", (c) => c.text("POST it"));
 
 const port = 3000;
