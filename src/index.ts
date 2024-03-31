d941a0e2b554ef16d496c001a2e9c250cb0a8090
aronteh.chess <aronteh.chess@gmail.com>
2024-03-31 15:08:13 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => c.text("GET it"));
+
+app.post("/", (c) => c.text("POST it"));
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
